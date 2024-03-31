d158795f6f76dee227edc589e50000ec2403b19e
adiniregina17 <117794795+adiniregina17@users.noreply.github.com>
2024-03-31 21:13:18 +0700

    Add files via upload

---
+++ tugaspr_modul3/tugaspr_modul3/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tugaspr_modul3.Models;
+
+namespace tugaspr_modul3.Controllers
+{
+    public class PersonController : Controller
+    {
+        private string __constr;
+
+        public PersonController(IConfiguration configuration)
+        {
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+
+        public IActionResult index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("api/person")]
+
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext(__constr);
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+        [HttpPost("api/person_auth"), Authorize]
+
+        public ActionResult<Person> ListPersonWithAuth()
+        {
+            PersonContext context = new PersonContext(this.__constr);
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+    }
+
+}
---
+++ tugaspr_modul3/tugaspr_modul3/Models/PersonContext.cs
@@ -0,0 +1,99 @@
+﻿using Npgsql;
+using tugaspr_modul3.Helpers;
+using tugaspr_modul3.Models;
+
+namespace tugaspr_modul3.Models
+{
+    public class PersonContext
+    {
+        private string __constr;
+        private string __ErrorMsg;
+
+        public PersonContext(string pConstr)
+        {
+            __constr = pConstr;
+        }
+        public List<Person> ListPerson()
+        {
+            List<Person> list1 = new List<Person>();
+            string query = string.Format(@"SELECT id_person, nama, alamat, email FROM users.person;");
+            SqlDBHelper db = new SqlDBHelper(this.__constr);
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list1.Add(new Person()
+                    {
+                        id_person = int.Parse(reader["id_person"].ToString()),
+                        nama = reader["nama"].ToString(),
+                        alamat = reader["alamat"].ToString(),
+                        email = reader["email"].ToString()
+                    });
+                }
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                __ErrorMsg = ex.Message;
+            }
+            return list1;
+
+        }
+        public void AddPerson(Person person)
+        {
+            string query = string.Format(@"INSERT INTO users.person (nama, alamat, email) VALUES ('{0}', '{1}', '{2}');",
+                person.nama, person.alamat, person.email);
+            SqlDBHelper db = new SqlDBHelper(this.__constr);
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                __ErrorMsg = ex.Message;
+            }
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            string query = string.Format(@"UPDATE users.person SET nama = '{0}', alamat = '{1}', email = '{2}' WHERE id_person = {3};",
+                person.nama, person.alamat, person.email, person.id_person);
+            SqlDBHelper db = new SqlDBHelper(this.__constr);
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                __ErrorMsg = ex.Message;
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            string query = string.Format(@"DELETE FROM users.person WHERE id_person = {0};", id);
+            SqlDBHelper db = new SqlDBHelper(this.__constr);
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                __ErrorMsg = ex.Message;
+            }
+        }
+    }
+
+}
